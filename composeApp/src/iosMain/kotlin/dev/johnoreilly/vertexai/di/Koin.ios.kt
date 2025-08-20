b50cc43c804492a05070d12ac39a79c2539341fb
John O'Reilly <oreillyj@gmail.com>
2025-08-20 09:42:55 +0100

    add missing koin file

---
+++ composeApp/src/iosMain/kotlin/dev/johnoreilly/vertexai/di/Koin.ios.kt
@@ -0,0 +1,16 @@
+package dev.johnoreilly.vertexai.di
+
+import dev.johnoreilly.vertexai.GenerativeModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+
+fun initialiseKoin(generativeModel: GenerativeModel) {
+    startKoin {
+        modules(
+            commonModule,
+            module { single<GenerativeModel> { generativeModel } }
+        )
+    }
+
+}
